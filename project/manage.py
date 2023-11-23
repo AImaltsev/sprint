f590a7844642738e400b0ae058c054d7fd509896
AImaltsev <boomer47.am@gmail.com>
2023-11-23 10:53:31 +0400

    бд в переменное окружение работает

--- project/manage.py
+++ project/manage.py
@@ -2,6 +2,20 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import dotenv, os
+
+dotenv.load_dotenv(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env'))
+
+from django.db import connection
+
+print("Database settings:")
+print(f"NAME: {connection.settings_dict['NAME']}")
+print(f"USER: {connection.settings_dict['USER']}")
+print(f"PASSWORD: {connection.settings_dict['PASSWORD']}")
+print(f"HOST: {connection.settings_dict['HOST']}")
+print(f"PORT: {connection.settings_dict['PORT']}")
+
+
 
 
 def main():
@@ -20,3 +34,12 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# Путь к каталогу bin в установке PostgreSQL
+pgsql_bin_path = r'C:\Program Files\PostgreSQL\16rc1\bin'
+
+# Добавить bin в переменную PATH
+os.environ['PATH'] = f'{pgsql_bin_path};{os.environ["PATH"]}'
+
+
