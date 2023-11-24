cb37ab4eb78eb83052f23236cda4f0e9d24b8a60
AImaltsev <boomer47.am@gmail.com>
2023-11-24 19:02:53 +0400

    Создал новые методы для спринт 2

--- project/pereval/urls.py
+++ project/pereval/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import SubmitDataView
+from .views import SubmitDataView, GetPerevalByIdView, EditPerevalView, GetUserPerevalsView
 
 urlpatterns = [
     path('submitData/', SubmitDataView.as_view(), name='submit_data'),
+    path('submitData/<int:id>/', GetPerevalByIdView.as_view(), name='get_pereval_by_id'),
+    path('submitData/<int:id>/edit/', EditPerevalView.as_view(), name='edit_pereval'),
+    path('submitData/user/', GetUserPerevalsView.as_view(), name='get_user_perevals'),
 ]
--- project/pereval/views.py
+++ project/pereval/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Coords, PerevalAdded, DatabaseHandler
+from .models import DatabaseHandler, PerevalAdded
 from .serializers import PerevalSerializer
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
 
 
 class SubmitDataView(APIView):
@@ -21,8 +24,46 @@ class SubmitDataView(APIView):
         db_handler = DatabaseHandler()
 
         # Вызываем метод добавления перевала
-        pereval = db_handler.add_pereval(beauty_title, title, other_titles, connect, add_time, coords_data, levels, raw_data)
+        pereval = db_handler.add_pereval(beauty_title, title, other_titles, connect, add_time, coords_data, levels,
+                                         raw_data)
 
         # Сериализуем созданный объект и возвращаем в ответе
         serializer = PerevalSerializer(pereval)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class GetPerevalByIdView(APIView):
+    def get(self, request, id, format=None):
+        # Получаем объект PerevalAdded по id
+        pereval = DatabaseHandler().get_pereval_by_id(id)
+
+        # Сериализуем объект и возвращаем в ответе
+        serializer = PerevalSerializer(pereval)
+        return Response(serializer.data)
+
+
+class EditPerevalView(APIView):
+    def patch(self, request, id, format=None):
+        # Получаем данные из запроса
+        data = request.data
+
+        # Пытаемся отредактировать запись
+        db_handler = DatabaseHandler()
+        result = db_handler.edit_pereval(id, data)
+
+        if result:
+            return Response({"state": 1}, status=status.HTTP_200_OK)
+        else:
+            return Response({"state": 0, "message": "Не удалось отредактировать запись"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GetUserPerevalsView(APIView):
+    def get(self, request, format=None):
+        # Получаем email из параметра запроса
+        user_email = request.query_params.get('user__email', '')
+
+        # Получаем все записи пользователя с указанным email
+        perevals = DatabaseHandler().get_user_perevals(user_email)
+
+        # Сериализуем записи и возвращаем в ответе
+        serializer = PerevalSerializer(perevals, many=True)
+        return Response(serializer.data)
